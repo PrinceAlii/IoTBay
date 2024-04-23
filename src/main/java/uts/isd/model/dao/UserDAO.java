a948cd53b5c4bfdeabf8e3ef2f68efdb849f7f77
Meghsoni17 <160580682+Meghsoni17@users.noreply.github.com>
2024-04-23 20:20:30 +1000

    Update UserDAO.java

--- src/main/java/uts/isd/model/dao/UserDAO.java
+++ src/main/java/uts/isd/model/dao/UserDAO.java
@@ -1,43 +1,33 @@
 package uts.isd.model.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import uts.isd.model.User;
+import javax.swing.plaf.nimbus.State;
+
+import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.xdevapi.PreparableStatement;
 
 public class UserDAO {
-    
     private Statement st;
-    private PreparedStatement readSt;
+    private PreparableStatement readSt;
     private String readQuery = "SELECT * FROM account";
 
-    public UserDAO (Connection connection) throws SQLException {
+    public UserDAO(Connection connection) throws SQL Exception {
         connection.setAutoCommit(true);
-        st = connection.createStatement();
         readSt = connection.prepareStatement(readQuery);
     }
 
-    public ArrayList<User> fetchUsers() throws SQLException {
-        ResultSet rs = readSt.executeQuery();
+    public ArrayList<User> fethUsers () throws SQLException {
+        Resultset rs = readSt.executeQuery();
 
         ArrayList<User> users = new ArrayList<User>();
-
-        while(rs.next()) {
-            String firstName = rs.getString(1);
-            String lastName = rs.getString(2);
-            User u = new User();
-            u.setName(firstName + " " + lastName);
-
-            System.out.println(u.getName());
-            
-            users.add(u);
+        while (rs.next()) {
+            String firstName = rs.getString
         }
-        
-        return users;
     }
-
+    
 }
