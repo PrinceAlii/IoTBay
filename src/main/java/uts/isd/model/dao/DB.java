5b2fc590a2bd44b91b01ddcd66372944f9027958
Ali B <30826345+PrinceAlii@users.noreply.github.com>
2024-04-23 20:13:07 +1000

    TUTORIAL WORK

---
+++ src/main/java/uts/isd/model/dao/DB.java
@@ -0,0 +1,14 @@
+package uts.isd.model.dao;
+
+import java.sql.Connection;
+
+public abstract class DB {
+    
+    protected String URL = "jdbc:mysql://localhost:3306/";
+    protected String db = "lab example";
+    protected String dbuser = "admin";
+    protected String dbpassword = "password";
+    protected String params = "?useSSL=false&allowPublicKeyRetrieval=true";
+    protected String driver = "com.mysql.cj.jdbc.Driver";
+    protected Connection conn;
+}
---
+++ src/main/java/uts/isd/model/dao/UserDAO.java
@@ -0,0 +1,43 @@
+package uts.isd.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import uts.isd.model.User;
+
+public class UserDAO {
+    
+    private Statement st;
+    private PreparedStatement readSt;
+    private String readQuery = "SELECT * FROM account";
+
+    public UserDAO (Connection connection) throws SQLException {
+        connection.setAutoCommit(true);
+        st = connection.createStatement();
+        readSt = connection.prepareStatement(readQuery);
+    }
+
+    public ArrayList<User> fetchUsers() throws SQLException {
+        ResultSet rs = readSt.executeQuery();
+
+        ArrayList<User> users = new ArrayList<User>();
+
+        while(rs.next()) {
+            String firstName = rs.getString(1);
+            String lastName = rs.getString(2);
+            User u = new User();
+            u.setName(firstName + " " + lastName);
+
+            System.out.println(u.getName());
+            
+            users.add(u);
+        }
+        
+        return users;
+    }
+
+}
