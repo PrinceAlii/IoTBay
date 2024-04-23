d81849d07533183cec52491a04d0072d90594fad
Meghsoni17 <160580682+Meghsoni17@users.noreply.github.com>
2024-04-23 20:29:59 +1000

    Addede my own dbuser

--- src/main/java/uts/isd/model/dao/DB.java
+++ src/main/java/uts/isd/model/dao/DB.java
@@ -5,8 +5,8 @@ import java.sql.Connection;
 public abstract class DB {
     
     protected String URL = "jdbc:mysql://localhost:3306/";
-    protected String db = "lab example";
-    protected String dbuser = "admin";
+    protected String db = "tuelab";
+    protected String dbuser = "megh";
     protected String dbpassword = "password";
     protected String params = "?useSSL=false&allowPublicKeyRetrieval=true";
     protected String driver = "com.mysql.cj.jdbc.Driver";
