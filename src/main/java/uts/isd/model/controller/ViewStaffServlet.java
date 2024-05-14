37a9ab258e5f43aafc12127b578b693dc7b7e41d
jujharsinghchahal <chahaljujharsingh55@gmail.com>
2024-05-14 15:14:20 +1000

    Update ViewStaffServlet.java

--- src/main/java/uts/isd/model/controller/ViewStaffServlet.java
+++ src/main/java/uts/isd/model/controller/ViewStaffServlet.java
@@ -2,57 +2,42 @@ package uts.isd.model.controller;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import uts.isd.model.Staff;
-import uts.isd.model.db.DBConnector;
+import uts.isd.model.dao.StaffDao;
+import uts.isd.model.dao.DaoFactory;
 
 public class ViewStaffServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
+        HttpSession session = request.getSession();
+        Connection conn = (Connection) session.getAttribute("conn");
+
+        // Create an instance of StaffDao using DaoFactory
+        StaffDao staffDao = DaoFactory.getStaffDao();
 
         try {
-            conn = DBConnector.getConnection();
-            stmt = conn.createStatement();
-            String query = "SELECT * FROM Staff";
-            rs = stmt.executeQuery(query);
+            // Fetch the staff members
+            ArrayList<Staff> staffList = staffDao.fetchStaff();
 
-            ArrayList<Staff> staffList = new ArrayList<>();
-            while (rs.next()) {
-                int userID = rs.getInt("userID");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-                String userType = rs.getString("userType");
-                String userAccount = rs.getString("userAccount");
-                boolean userStatus = rs.getBoolean("userStatus");
-
-                Staff staff = new Staff(userID, name, email, userType, userAccount, userStatus);
-                staffList.add(staff);
-            }
-
+            // Set the staff list as a request attribute
             request.setAttribute("staffList", staffList);
-            request.getRequestDispatcher("ViewStaff.jsp").forward(request, response);
+
+            // Forward the request to the ViewStaff.jsp
+            request.getRequestDispatcher(" ViewStaff.jsp").forward(request, response);
         } catch (SQLException e) {
-            System.err.println("An error occurred while fetching staff members: " + e.getMessage());
-        } finally {
-            try {
-                if (rs != null) rs.close();
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            // Handle the SQLException here
+            System.err.println("An error occurred while fetching staff: " + e.getMessage());
+            // You can also display a user-friendly message to the user on the JSP
+            // by setting a request attribute here
         }
     }
 }
